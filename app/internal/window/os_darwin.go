3a940a5df9349b6fcd01015a6830f591a9c5085c
Larry Clapp <larry@theclapp.org>
2020-08-27 10:21:52 -0400

    Do gio_createDisplayLink in a loop up to 10 times

--- app/internal/window/os_darwin.go
+++ app/internal/window/os_darwin.go
@@ -17,6 +17,8 @@ __attribute__ ((visibility ("hidden"))) void gio_setDisplayLinkDisplay(CFTypeRef
 import "C"
 import (
 	"errors"
+	"log"
+	"runtime/debug"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -89,7 +91,19 @@ func NewDisplayLink(callback func()) (*displayLink, error) {
 		states:   make(chan bool),
 		dids:     make(chan uint64),
 	}
-	dl := C.gio_createDisplayLink()
+	var dl C.CFTypeRef
+	for n, sleep := 0, time.Millisecond; n < 11; n, sleep = n+1, sleep*2 {
+		dl = C.gio_createDisplayLink()
+		if dl != 0 {
+			break
+		}
+		log.Printf("app: failed to create display link: %d of 10", n)
+		debug.PrintStack()
+		log.Printf("stack finished")
+		if n < 10 {
+			time.Sleep(sleep)
+		}
+	}
 	if dl == 0 {
 		return nil, errors.New("app: failed to create display link")
 	}
